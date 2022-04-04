6f2b72ae2edd60e89c2a9c3d4cbbe0cd30d93c84
Ben Zimmerman <59181438+T3CH3Y@users.noreply.github.com>
2022-04-04 00:53:02 -0400

    Delete sussygif directory

--- sussygif/input.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-gif = Image.open("input.gif")
-print("Number of frames: " + str(gif.n_frames))
-for i in range(gif.n_frames):
-    gif.seek(i)
-    gif.save(f"frames/frame{i}.png")
